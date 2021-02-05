8bafaa9d1fa741cf895341063a4d05378c04e6dc
OtariOboladze <otari.oboladze@outlook.com>
2021-02-05 13:02:21 +0100

    start c sharp projects

---
+++ NET_project/NET_project/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+class camelCaseCadena
+{
+
+    static void Main()
+    {
+
+        // Define the string by input.
+        string myString = Console.ReadLine();
+
+        // Create a TextInfo based on the "en-US" culture.
+        TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+
+        // Change a string to titlecase.
+        Console.WriteLine("\"{0}\" : {1}", myString, myTI.ToTitleCase(myString));
+    }
+}
+
---
+++ console_app/console_app/Program.cs
@@ -0,0 +1,38 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}*/
+
+
+
+using System;
+using System.Globalization;
+
+class camelCaseCadena
+{
+
+    static void Main()
+    {
+        while (true){
+            // Define the string by input.
+            string myString = Console.ReadLine();
+
+            // Create a TextInfo based on the "en-US" culture.
+            TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+
+            // Change a string to titlecase.
+            Console.WriteLine("\"{0}\" : {1}", myString, myTI.ToTitleCase(myString));
+        }
+    }
+}
